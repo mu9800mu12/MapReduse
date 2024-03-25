1a6f5da21126639e90d6f7834437d66d2ee76aa4
Song <mu9800mu12@naver.com>
2024-03-25 14:57:11 +0900

    맵리듀스 공부한 것

--- src/main/java/ip/IPCountReducer.java
+++ src/main/java/ip/IPCountReducer.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 public class IPCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
 
 
+
     @Override
     public void reduce(Text key, Iterable<IntWritable> values, Context context)
         throws IOException, InterruptedException {
